0656b6aa684fa56efbaf0257b6dbb4f2ac137d32
Daniel Moura <daniel.mlds@gmail.com>
2022-03-12 15:09:55 -0300

    Pagination

--- frontend/src/components/FormCard/index.tsx
+++ frontend/src/components/FormCard/index.tsx
@@ -18,7 +18,7 @@ function FormCard( { movieId } : Props) {
         axios.get(`${BASE_URL}/movies/${movieId}`).then(response => {
             setMovie(response.data);
         });
-    },[movieId]);
+    },[movieId])
 
 
     return (
